28040bcc4599c953ae34343ea3b5d3508cf4a1a5
Matthew Olde <mattolde@gmail.com>
2014-09-08 15:42:33 +1000

    adding testing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1','Item 2','Item 3'];
+    $scope.todos = [];
 
     $scope.addTodo = function(){
       if($scope.todo.length > 0){
